e875d20199bac8dc1b16a96ad0a69776c91476dc
nirmalwsolus <nirmal@wsolus.com>
2017-12-10 18:32:02 +0530

    Adding navigation

--- App.js
+++ App.js
@@ -1,42 +1,65 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, List, ListItem, Text, Body, Title, Icon, Right} from 'native-base';
-export default class ListExample extends Component {
+import { Container, Header, Content, Body, Title,List, ListItem, Text, Icon, Right, Left} from 'native-base';
+import {HashTags} from './components';
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+
+
+class Categories extends Component {
+  static navigationOptions = {
+    title: 'Categories',
+    headerStyle: {
+      backgroundColor: 'black',
+      
+    },
+    headerTintColor: '#fff',
+  };
   render() {
+    
+    const { navigate } = this.props.navigation;
     return (
       <Container>
-        <Header style={styles.header} androidStatusBarColor="black">
-        <Body>
-            <Title>Categories</Title>
-          </Body>
-        </Header>
+      
         <Content>
-          <List>
-            <ListItem button>
-              
-              <Text>Simon Mignolet</Text>
-              <Right style={styles.right}>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem button>
-              <Text>Nathaniel Clyne</Text>
-              <Right style={styles.right}>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem button>
-              <Text>Dejan Lovren</Text>
-              <Right style={styles.right}>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-          </List>
+        <List style={{ flex: 1, backgroundColor: '#fff' }}>
+
+                        <ListItem button onPress={() =>navigate('HashTags') }>
+                        
+                            <Text>Simons Mignolet</Text>
+                            <Right style={styles.right}>
+                                <Icon name="arrow-forward" />
+                            </Right>
+                        </ListItem>
+                        <ListItem button>
+                            <Text>Nathaniel Clyne</Text>
+                            <Right style={styles.right}>
+                                <Icon name="arrow-forward" />
+                            </Right>
+                        </ListItem>
+                        <ListItem button>
+                            <Text>Dejan Lovren</Text>
+                            <Right style={styles.right}>
+                                <Icon name="arrow-forward" />
+                            </Right>
+                        </ListItem>
+                    </List>
         </Content>
       </Container>
     );
   }
 }
+const InstaHashes = StackNavigator({
+  Home: { screen: Categories },
+  HashTags: { screen: HashTags }
+});
 
+export default class App extends React.Component {
+  render() {
+    return <InstaHashes />;
+  }
+}
 const styles = {
     header:{
       backgroundColor:'black'
@@ -44,7 +67,7 @@ const styles = {
     right:{
       flex:1,
       justifyContent: 'flex-end',
-     
-    }
-    
-}
+
+  }
+
+};
